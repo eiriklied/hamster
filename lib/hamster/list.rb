83b9f269c41670d7d17a88cf5377678ecbeab976
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-12-13 19:27:17 -0800

    We don't need to reference self

--- lib/hamster/list.rb
+++ lib/hamster/list.rb
@@ -167,7 +167,7 @@ module Hamster
     def_delegator :self, :cons, :>>
 
     def add(item)
-      self.append(Hamster.list(item))
+      append(Hamster.list(item))
     end
     def_delegator :self, :add, :<<
 
@@ -270,7 +270,7 @@ module Hamster
     def cycle
       Stream.new do
         next self if empty?
-        Sequence.new(head, tail.append(self.cycle))
+        Sequence.new(head, tail.append(cycle))
       end
     end
 
@@ -325,7 +325,7 @@ module Hamster
     def_delegator :self, :uniq, :remove_duplicates
 
     def union(other)
-      self.append(other).uniq
+      append(other).uniq
     end
     def_delegator :self, :union, :|
 
